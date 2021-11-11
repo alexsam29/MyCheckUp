11e70a1bb6b393997374b92dc4653ebfb53b9625
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-11-10 19:00:06 -0500

    Backend - Server set up Initial set up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import { router } from './routes/router'
+import { notFoundHandler } from './middleware/not-found-handler'
+import { errorHandler } from './middleware/error-handler'
+import { PORT } from './common/constants'
 
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(router)
+app.use(notFoundHandler)
+app.use(errorHandler)
+
+const main = async () => {
+   try {
+      app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+   }
+   catch (err: unknown) {
+      console.error(err)
+   }
+}
+
+main()
