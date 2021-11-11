3291363086d739a2a0d676f248e54c1f5faac0e7
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-11-11 12:25:47 -0500

    Backend - New Model Role enum: specifies user roles for the API

---
+++ backend/src/models/role.ts
@@ -0,0 +1,13 @@
+/**
+ * Role reflects different authorization level.
+ * 
+ * Roles:
+ * - `ADMIN`
+ * - `DOCTOR`
+ * - `PATIENT`
+ */
+ export enum Role {
+   ADMIN = 'admin',
+   DOCTOR = 'doctor',
+   PATIENT = 'patient'
+}
