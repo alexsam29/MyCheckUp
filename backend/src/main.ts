5c0f632d495a1922de7aa1de372e243504a1cea3
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-11-11 09:46:05 -0500

    Backend - Server Set Up Added connection to the database, log to logger instead of console

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
+import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
+import config from 'config'
+import { createConnection, ConnectionOptions } from 'typeorm'
 import { router } from './routes/router'
 import { notFoundHandler } from './middleware/not-found-handler'
 import { errorHandler } from './middleware/error-handler'
-import { PORT } from './common/constants'
+import { PORT, PROD } from './common/constants'
+import { logger } from './common/logger'
 
 const app = express()
 app.use(express.json())
@@ -14,10 +18,23 @@ app.use(errorHandler)
 
 const main = async () => {
    try {
-      app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+      // Database Connection:
+      await createConnection({
+         type: config.get('database.type'),
+         host: config.get('database.host'),
+         port: config.get('database.port'),
+         username: config.get('database.username'),
+         password: config.get('database.password'),
+         database: config.get('database.name'),
+         entities: config.get('typeorm.entities'),
+         synchronize: !PROD,
+         logging: false
+      } as ConnectionOptions)
+
+      app.listen(PORT, () => logger.info(`Server started on port ${PORT}`))
    }
    catch (err: unknown) {
-      console.error(err)
+      logger.error(err)
    }
 }
 
