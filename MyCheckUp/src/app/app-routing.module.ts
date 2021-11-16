ad0b9720b12d7bd6397e0410df5711145d188124
atif <atif@Atifs-MacBook-Pro.local>
2021-11-15 20:01:21 -0500

    sign-up page route

--- MyCheckUp/src/app/app-routing.module.ts
+++ MyCheckUp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   {path: 'signin', component: SignInComponent},
+  {path: 'createaccount',component:CreateAccountComponent},
   {path: '', component: LandingPageComponent, pathMatch: 'full'}
 ];
 
