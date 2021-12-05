680b8aa64f6e84b2e358221ee860302fb4d156a5
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-12-05 14:56:12 -0500

    feat(backend): added global type overrides

---
+++ backend/src/@types/index.d.ts
@@ -0,0 +1,11 @@
+import 'express-session'
+
+// Global type overrides & extensions:
+
+declare module 'express-session' { 
+   interface SessionData {
+      valid: boolean
+      userId: string
+      role: string
+   }
+}
