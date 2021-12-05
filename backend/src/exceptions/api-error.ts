6706ac48851770667cda0f0204ace389c55c51ce
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-12-05 16:31:34 -0500

    feat(backend): improved error-handling
    Added details array to the ApiError.

--- backend/src/exceptions/api-error.ts
+++ backend/src/exceptions/api-error.ts
@@ -5,18 +5,24 @@
  */
 export class ApiError extends Error {
    status: number
+   details: any[]
 
-   constructor(status: number, message: string) {
+   constructor(status: number, message: string, details: any[] = []) {
       super(message)
       this.status = status
+      this.details = details
    }
 
-   static BadRequest(message: string) {
-      return new ApiError(400, message)
+   static BadRequest(message: string, details: any[] = []) {
+      return new ApiError(400, message, details)
    }
 
    static Unauthorized(message?: string) {
       return new ApiError(401, message || 'Not Authorized')
+   }
+
+   static Forbidden(message?: string) {
+      return new ApiError(403, message || 'Forbidden')
    }
 
    static NotFound(message: string) {
