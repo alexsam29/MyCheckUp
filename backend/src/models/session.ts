f9efe469a2b79a657f7ad079259d4a1a20aa61d4
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-12-05 16:33:10 -0500

    feat(backend): implemented session store
    Added Session model.
    Implemented userSession middleware.

---
+++ backend/src/models/session.ts
@@ -0,0 +1,34 @@
+import {
+   Entity,
+   PrimaryColumn,
+   Column,
+   Index
+} from 'typeorm'
+import { ISession } from 'connect-typeorm'
+
+/**
+ * User Session model.
+ * 
+ * Fields:
+ * - `expiredAt`
+ * - `id`
+ * - `json`
+ */
+@Entity()
+export class Session implements ISession {
+   @Index()
+   @Column({
+      type: 'bigint',
+      default: Date.now()
+   })
+   expiredAt!: number
+
+   @PrimaryColumn({
+      type: 'varchar',
+      length: 255
+   })
+   id!: string
+
+   @Column('text')
+   json!: string
+}
