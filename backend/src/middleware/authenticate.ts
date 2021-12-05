5a1a0e0aa6f07dd953eecfa523ec9b80b0e5522d
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-12-05 16:33:39 -0500

    feat(backend): implemented authentication and authorization

---
+++ backend/src/middleware/authenticate.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+import { ApiError } from '../exceptions/api-error'
+
+/**
+ * Middleware for securing routes.  
+ * Authenticates user by validating session cookie.
+ */
+export const authenticate = (req: Request, _: Response, next: NextFunction) => {
+   if (!req.session || !req.session.valid) {
+      return next(ApiError.Unauthorized())
+   }
+
+   return next()
+}
