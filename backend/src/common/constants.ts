06b818468374e053d02a6265994e57217a318c9e
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-19 15:10:04 -0500

    chore(api): change version to 0.4.0

--- backend/src/common/constants.ts
+++ backend/src/common/constants.ts
@@ -3,7 +3,7 @@ import path from 'path'
 export const PROD = process.env.NODE_ENV === 'production'
 export const PORT = process.env.PORT || 5000
 export const LOG_DIR = path.join(__dirname, '../../logs')
-export const VERSION = '0.3.0'
+export const VERSION = '0.4.0'
 
 // Session:
 export const SESSION_COOKIE = 'MCUSessionId'
