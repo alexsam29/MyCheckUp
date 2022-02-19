8fb3b96f6bb1de3eecc895eb3789624d163a436e
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-19 08:41:24 -0500

    feat(api): added OpenAPI specification

---
+++ backend/src/common/open-api-spec.ts
@@ -0,0 +1,15 @@
+import jsdoc from 'swagger-jsdoc'
+import { VERSION } from './constants'
+
+const options: jsdoc.Options = {
+   definition: {
+      openapi: '3.0.0',
+      info: {
+         title: 'MyCheckUp Api',
+         version: VERSION,
+      },
+   },
+   apis: ['./dist/routes/*.js']
+}
+
+export const openApiSpec = jsdoc(options)
