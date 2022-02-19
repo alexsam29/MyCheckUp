a434652704ab4291798b2845fd4a3a8ec19af431
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-19 09:40:52 -0500

    feat(api): added doctor registration route

--- backend/src/routes/doctor-router.ts
+++ backend/src/routes/doctor-router.ts
@@ -9,11 +9,34 @@ export const DoctorRouter = express.Router()
 
 
 DoctorRouter.post('/doctor/login',
-   body('email').notEmpty().isEmail(),
-   body('password').notEmpty().isLength({ min: 6, max: 50 }),
+   body('email').isEmail(),
+   body('password').isLength({ min: 6, max: 50 }),
    DoctorController.login)
 
 DoctorRouter.post('/doctor/logout', DoctorController.logout)
+
+/**
+ * @openapi
+ * /doctor/register:
+ *   post:
+ *     summary: register doctor
+ *     tags:
+ *       - Doctor
+ *     description: Register doctor account. Account will be inactive until approved.
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+DoctorRouter.post('/doctor/register',
+   body('email').isEmail(),
+   body('password').isLength({ min: 6, max: 50 }),
+   body('firstName').trim().isLength({ min: 1, max: 50}),
+   body('lastName').trim().isLength({ min: 1, max: 50}),
+   body('license').optional().trim().isLength({ min: 1, max: 100 }),
+   body('specialty').optional().trim().isLength({ min: 1, max: 100 }),
+   body('title').optional().trim().isLength({ min: 1, max: 100 }),
+   body('phoneNumber').optional().trim().isLength({ min: 10, max: 20 }),
+   DoctorController.register)
 
 /**
  * @openapi
