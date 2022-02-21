dc26d022ccf19b2f07037336c2bfcaf6fbac9088
Atif Ali <aali309@myseneca.ca>
2022-02-21 17:30:51 -0500

    added appointment page

--- MyCheckUp/src/app/book-appointment/book-appointment.component.ts
+++ MyCheckUp/src/app/book-appointment/book-appointment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-book-appointment',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-appointment.component.css']
 })
 export class BookAppointmentComponent implements OnInit {
+  fail: boolean = false;
+  success: boolean = false;
+  loading: boolean = true;
+  message: string = '';
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  onSubmit(bookAppForm: NgForm) {
+    console.log(bookAppForm.value.firstName);
+    if (bookAppForm.valid) {
+      this.userService
+        .book(
+          bookAppForm.value.firstName,
+          bookAppForm.value.lastName,
+          bookAppForm.value.phone,
+          bookAppForm.value.dateOfApp,
+          bookAppForm.value.timeOfApp,
+          bookAppForm.value.prefDoc,
+          bookAppForm.value.reasonOfApp
+        )
+        .subscribe(
+          (data) => {
+            this.success = true;
+            this.message = 'Appointment Successfully Booked!';
+            setTimeout(() => {
+              this.router.navigate(['signin']);
+            }, 3000);
+          },
+          (error) => {
+            console.log(error);
+            this.message = error.error.error;
+            this.fail = true;
+          }
+        );
+    }
   }
-
 }
--- MyCheckUp/src/app/services/user.service.ts
+++ MyCheckUp/src/app/services/user.service.ts
@@ -69,6 +69,35 @@ url = 'http://localhost:5000/';
         })
       );
   }
+  
+  book(
+    first: string,
+    last: string,
+    date: string,
+    phone: string,
+    time: string,
+    doc: string,
+    reason: string 
+  ) {
+    return this.http
+      .post<any>(this.url + 'patient/bookApp', {
+        firstName: first,
+        lastName: last,
+        dateOfApp: date,
+        phone: phone,
+        timeOfApp: time,
+        prefDoc: doc,
+        reasonOfApp: reason
+      }, {withCredentials: true})
+      .pipe(
+        map((res) => {
+          if (res) {
+            return true;
+          }
+          return false;
+        })
+      );
+  }
 
   getProfile(): Observable<any>{
     return this.http
