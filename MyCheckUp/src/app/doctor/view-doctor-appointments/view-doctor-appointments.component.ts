576e8fd225704c1282f776be33b1154834738e9e
alexsam29 <69481177+alexsam29@users.noreply.github.com>
2022-03-29 04:51:04 -0400

    Added empty view doctor appointments component

---
+++ MyCheckUp/src/app/doctor/view-doctor-appointments/view-doctor-appointments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-doctor-appointments',
+  templateUrl: './view-doctor-appointments.component.html',
+  styleUrls: ['./view-doctor-appointments.component.css']
+})
+export class ViewDoctorAppointmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
