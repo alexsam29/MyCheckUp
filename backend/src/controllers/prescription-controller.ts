b0ee0fc34b1abd4137bf81c29843dd5dde3ea299
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-04-12 17:37:51 -0400

    feat(): POST /prescriptions

--- backend/src/controllers/prescription-controller.ts
+++ backend/src/controllers/prescription-controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
+import { validationResult } from 'express-validator'
 import { PrescriptionService } from '../services/prescription-service'
+import { ApiError } from '../exceptions/api-error'
+import { PrescriptionStatus } from 'models/prescription-status'
 
 export const PrescriptionController = {
    async getPatientPrescription(req: Request, res: Response, next: NextFunction) {
@@ -10,6 +13,38 @@ export const PrescriptionController = {
             patientId,
          })
          return res.status(200).json(prescriptions)
+      } catch (err: unknown) {
+         return next(err)
+      }
+   },
+
+   async create(req: Request, res: Response, next: NextFunction) {
+      try {
+         const errors = validationResult(req)
+         if (!errors.isEmpty()) {
+            throw ApiError.BadRequest(
+               'Invalid data in the request body',
+               errors.array()
+            )
+         }
+
+         const date = new Date(req.body.expiryDate)
+         const status =
+            req.session.role === 'patient'
+               ? PrescriptionStatus.PENDING
+               : req.body.status
+
+         const prescription = await PrescriptionService.create({
+            patientId: req.body.patientId,
+            doctorId: req.body.doctorId,
+            description: req.body.description,
+            numOfRefill: req.body.numOfRefill,
+            expiryDate: date,
+            status: status,
+            requestedByPatient: req.session.role === 'patient',
+         })
+
+         return res.status(200).json(prescription)
       } catch (err: unknown) {
          return next(err)
       }
--- backend/src/routes/prescription-router.ts
+++ backend/src/routes/prescription-router.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { body } from 'express-validator'
 import { authorize } from '../middleware/authorize'
 import { Role } from '../models/role'
 import { PrescriptionController } from '../controllers/prescription-controller'
@@ -26,4 +27,31 @@ PrescriptionRouter.get(
    '/patient/prescriptions',
    authorize([Role.PATIENT, Role.DOCTOR, Role.ADMIN]),
    PrescriptionController.getPatientPrescription
+)
+
+/**
+ * @openapi
+ * /prescriptions:
+ *   post:
+ *     summary: register admin
+ *     tags:
+ *       - Prescription
+ *     description: Create prescrition either as Doctor or as Patient.
+ *     security:
+ *       - cookieAuth: []
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+PrescriptionRouter.post(
+   '/prescriptions',
+   authorize([Role.DOCTOR, Role.PATIENT]),
+   body('patientId').notEmpty().isUUID,
+   body('doctorId').notEmpty().isUUID,
+   body('description').isString().isLength({ min: 1, max: 255 }),
+   body('numOfRefill').notEmpty().isInt(),
+   body('expiryDate').notEmpty(),
+   body('status').notEmpty(),
+   body('requestedByPatient').notEmpty().isBoolean(),
+   PrescriptionController.create
 )
