4c46e761c6b8b3e923766e89365875fda2c7c979
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-04-12 15:40:20 -0400

    feat(): PrescriptionService find, findById, findByPatientDoctor

--- backend/src/services/prescription-service.ts
+++ backend/src/services/prescription-service.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm'
 import { Prescription } from '../models/prescription'
 import { PrescriptionStatus } from '../models/prescription-status'
+import { SearchQuery } from './utils/search-query'
 import { PatientService } from './patient-service'
 import { DoctorService } from './doctor-service'
 import { ApiError } from '../exceptions/api-error'
@@ -14,17 +15,17 @@ export const PrescriptionService = {
     * @returns Created prescription.
     */
    async create(dto: {
-      patientId: string,
-      doctorId: string,
-      description: string,
-      numOfRefill: number,
-      expiryDate: Date,
-      status: PrescriptionStatus,
+      patientId: string
+      doctorId: string
+      description: string
+      numOfRefill: number
+      expiryDate: Date
+      status: PrescriptionStatus
       requestedByPatient: boolean
    }): Promise<Prescription> {
       const [patient, doctor] = await Promise.all([
          PatientService.findOne({ id: dto.patientId }),
-         DoctorService.findOne({ id: dto.doctorId})
+         DoctorService.findOne({ id: dto.doctorId }),
       ])
 
       if (!patient) {
@@ -46,12 +47,16 @@ export const PrescriptionService = {
       return getRepository(Prescription).save(presc)
    },
 
+   /**
+    * @param dto Prescription id and updated information.
+    * @returns Updated prescription.
+    */
    async update(dto: {
-      id: string,
-      description?: string,
-      numOfRefill?: number,
-      expiryDate?: Date,
-      status?: PrescriptionStatus,
+      id: string
+      description?: string
+      numOfRefill?: number
+      expiryDate?: Date
+      status?: PrescriptionStatus
       requestedByPatient?: boolean
    }): Promise<Prescription> {
       const presc = await getRepository(Prescription).findOne(dto.id)
@@ -69,14 +74,80 @@ export const PrescriptionService = {
       return getRepository(Prescription).save(presc)
    },
 
-   async find() {
-      
+   /**
+    * Find prescriptions in the database that match given conditions.
+    *
+    * @param searchBy Search condition (optional).
+    * @param exact Matches exactly if true (optional, defaul false).
+    * @param offset Search offset (optional).
+    * @param limit Maximum number of accounts to be returned (optional, default 100).
+    * @returns Array of found prescriptions.
+    */
+   async find(
+      searchBy: {
+         patientId?: string
+         doctorId?: string
+         description?: string
+         numOfRefill?: number
+         expiryDate?: Date
+         status?: PrescriptionStatus
+         requestedByPatient?: boolean
+      },
+      options?: SearchQuery
+   ): Promise<Prescription[]> {
+      const prescriptions = await getRepository(Prescription).find({
+         where: searchBy,
+         order: { createdAt: 'DESC' },
+         skip: options?.offset,
+         take: options?.limit,
+      })
+
+      if (!prescriptions || !prescriptions.length) {
+         throw ApiError.NotFound('No prescriptions were found')
+      }
+
+      return prescriptions
    },
 
-   async findOne() {
+   /**
+    * @param id Prescription id.
+    * @returns Found prescription.
+    */
+   async findById(id: string): Promise<Prescription> {
+      const presc = await getRepository(Prescription).findOne(id)
 
+      if (!presc) {
+         throw ApiError.NotFound('Prescription not found')
+      }
+
+      return presc
    },
 
+   /**
+    * @param patientId Associated patient id.
+    * @param doctorId Associated doctor id.
+    * @returns Found prescription.
+    */
+   async findByPatientDoctor(
+      patientId: string,
+      doctorId: string
+   ): Promise<Prescription> {
+      const presc = await getRepository(Prescription).findOne({
+         patientId,
+         doctorId,
+      })
+
+      if (!presc) {
+         throw ApiError.NotFound('Prescription not found')
+      }
+
+      return presc
+   },
+
+   /**
+    * @param id Prescription id.
+    * @returns Removed prescription.
+    */
    async remove(id: string): Promise<Prescription> {
       const presc = await getRepository(Prescription).findOne(id)
 
@@ -85,5 +156,5 @@ export const PrescriptionService = {
       }
 
       return getRepository(Prescription).remove(presc)
-   }
+   },
 }
