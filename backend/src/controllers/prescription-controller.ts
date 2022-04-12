bb0c75f8de2e2c9a35b8b677a1fa32fa87536b41
Rad Eshghi <reshghi@myseneca.ca>
2022-04-12 15:28:37 -0400

    get patient prescription added

--- backend/src/controllers/prescription-controller.ts
+++ backend/src/controllers/prescription-controller.ts
@@ -1,11 +1,17 @@
-
+import { Request, Response, NextFunction } from 'express'
+import { PrescriptionService } from '../services/prescription-service'
 
 export const PrescriptionController = {
+   async getPatientPrescription(req: Request, res: Response, next: NextFunction) {
+      try {
+         const { userId: patientId } = req.session
 
-
-
-
-    
-
-
+         const prescriptions = await PrescriptionService.findPrescription({
+            patientId,
+         })
+         return res.status(200).json(prescriptions)
+      } catch (err: unknown) {
+         return next(err)
+      }
+   },
 }
--- backend/src/services/prescription-service.ts
+++ backend/src/services/prescription-service.ts
@@ -1,16 +1,34 @@
-
-
+import { ApiError } from '../exceptions/api-error'
+import { Prescription } from '../models/prescription'
+import { getRepository } from 'typeorm'
 
 export const PrescriptionService = {
+   async findPrescription(
+      searchBy?: { patientId?: string },
+      offset = 0,
+      limit = 100
+   ): Promise<Prescription[]> {
+      const repository = getRepository(Prescription)
 
+      const prescription = await repository.find({
+         select: [
+            'id',
+            'patientId',
+            'doctorId',
+            'description',
+            'numOfRefill',
+            'expiryDate',
+            'status',
+            'RequestedBy',
+         ],
+         where: searchBy,
+         skip: offset,
+         take: limit,
+      })
 
-    
+      if (!prescription)
+         throw ApiError.NotFound('No prescription found for this patient!')
 
-
-
-
-
-
-
-
+      return prescription
+   },
 }
